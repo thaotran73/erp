64069a2ae1975e6f1075e0dfb0f28182b50f7264
Thao Tran <ttpthaoqt@gmail.com>
2018-09-29 13:53:14 +0700

    Remove generate files

--- erp-core/wwwroot/build/app/vendor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* This file is mainly used for importing third party scripts */
-Object.defineProperty(exports, "__esModule", { value: true });
-require("jquery");
-require("popper.js");
-//# sourceMappingURL=vendor.js.map
