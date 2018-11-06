e50a17b59d0204d9d1fdff3451540e8c01e440ac
Thai Tran <tranhuynhthai@yahoo.com>
2018-11-06 15:48:35 +0700

    Update code ts

--- erp/ClientApp/app/routes/warehouse/mainmaterial.module.ts
+++ erp/ClientApp/app/routes/warehouse/mainmaterial.module.ts
@@ -1,7 +1,9 @@
 ﻿import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular/main';
-import { NgxSelectModule } from 'ngx-select-ex'
+import { NgxSelectModule, INgxSelectOptions } from 'ngx-select-ex';
+import { NgSelectModule } from '@ng-select/ng-select';
+
 
 import { SharedModule } from '../../shared/shared.module';
 import { MainmaterialComponent } from './mainmaterial/mainmaterial.component';
@@ -16,6 +18,7 @@ const routes: Routes = [
         RouterModule.forChild(routes),
         AgGridModule.withComponents([MainmaterialComponent]),
         NgxSelectModule,
+        NgSelectModule
     ],
 
     declarations: [MainmaterialComponent],
--- erp/ClientApp/app/routes/warehouse/mainmaterial/mainmaterial.component.ts
+++ erp/ClientApp/app/routes/warehouse/mainmaterial/mainmaterial.component.ts
@@ -1,8 +1,18 @@
 ﻿import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GridOptions } from 'ag-grid/main';
 import { HttpClient } from '@angular/common/http';
-import * as _ from 'lodash';
-declare var $: any;
+
+interface Person {
+    id: string;
+    isActive: boolean;
+    age: number;
+    name: string;
+    gender: string;
+    company: string;
+    email: string;
+    phone: string;
+    disabled?: boolean;
+}
 
 @Component({
     selector: 'app-mainmaterial',
@@ -18,16 +28,8 @@ export class MainmaterialComponent implements OnInit, OnDestroy {
     gridOptions: GridOptions;
 
     // ng2Select
-    public items: Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
-        'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
-        'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
-        'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
-        'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
-        'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
-        'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
-        'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
-        'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
-        'Zagreb', 'Zaragoza', 'Łódź'];
+    public items: Person[] = [];
+
     public value: any = {};
     public _disabledV: string = '0';
     public disabled: boolean = false;
@@ -53,108 +55,74 @@ export class MainmaterialComponent implements OnInit, OnDestroy {
         Uruguay: "uy"
     };
 
-    // Filter Example
-    irishAthletes = ['John Joe Nevin', 'Katie Taylor', 'Paddy Barnes', 'Kenny Egan', 'Darren Sutherland', 'Margaret Thatcher', 'Tony Blair', 'Ronald Regan', 'Barack Obama'];
-
-    columnDefsFilter = [{
-        headerName: 'Athlete',
-        field: 'athlete',
-        width: 150,
-        filter: 'set',
-        filterParams: {
-            cellHeight: 20,
-            values: this.irishAthletes
-        }
-    }, {
-        headerName: 'Age',
-        field: 'age',
-        width: 90,
-        filter: 'number',
-
-    }, {
-        headerName: 'Country',
-        field: 'country',
-        width: 120,
-            cellRenderer: countryCellRenderer,
-            //pinned: true,
-            filter: 'set',
-            filterParams: {
-                cellRenderer: countryCellRenderer,
-                cellHeight: 20
-            },
-            cellEditor: 'agRichSelectCellEditor',
-            cellEditorParams: {
-                values: ["Argentina", "Brazil", "Colombia", "France", "Germany", "Greece", "Iceland", "Ireland",
-                    "Italy", "Malta", "Portugal", "Norway", "Peru", "Spain", "Sweden", "United Kingdom",
-                    "Uruguay", "Venezuela", "Belgium", "Luxembourg"],
-                cellRenderer: countryCellRenderer,
-            },
-            editable: true
-    }, {
-        headerName: 'Year',
-        field: 'year',
-        width: 90,
-        filter: 'text',
-        editable: true
-    }, {
-        headerName: 'Date',
-        field: 'date',
-        width: 110
-    }, {
-        headerName: 'Sport',
-        field: 'sport',
-        width: 110
-    }, {
-        headerName: 'Gold',
-        field: 'gold',
-        width: 100,
-        filter: 'number'
-    }, {
-        headerName: 'Silver',
-        field: 'silver',
-        width: 100,
-        filter: 'number'
-    }, {
-        headerName: 'Bronze',
-        field: 'bronze',
-        width: 100,
-        filter: 'number'
-    }, {
-        headerName: 'Total',
-        field: 'total',
-        width: 100,
-        filter: 'number'
-    }];
+    columnHeader = 
+        [{
+            headerName: 'Athlete',
+            field: 'athlete',
+            width: 150
+        }, {
+            headerName: 'Age',
+            field: 'age',
+            width: 90
+        }, {
+            headerName: 'Country',
+            field: 'country',
+            width: 120
+        }, {
+            headerName: 'Year',
+            field: 'year',
+            width: 90
+        }, {
+            headerName: 'Date',
+            field: 'date',
+            width: 110
+        }, {
+            headerName: 'Sport',
+            field: 'sport',
+            width: 110
+        }, {
+            headerName: 'Gold',
+            field: 'gold',
+            width: 100
+        }, {
+            headerName: 'Silver',
+            field: 'silver',
+            width: 100
+        }, {
+            headerName: 'Bronze',
+            field: 'bronze',
+            width: 100
+        }, {
+            headerName: 'Total',
+            field: 'total',
+            width: 100
+        }];
 
     constructor(http: HttpClient) {
 
         // Load from JSON
-        http.get<any>('assets/server/ag-owinners-header.json')
+        http.get<any>('assets/server/people.json')
             .subscribe((data) => {
-                console.log(data);
-                let columnDefsFilter = data;
+                console.log(data);                
+                this.items = data;
             });
 
         // Filter example
         this.gridOptions = <GridOptions>{
             headerHeight: 40,
-            columnDefs: this.columnDefsFilter,
+            columnDefs: this.columnHeader,
             rowData: null,
             enableFilter: true,
             defaultColDef: { editable: true },
             singleClickEdit: true
         };
+
         // Load from JSON
         http.get<any>('assets/server/ag-owinners.json')
             .subscribe((data) => {
                 this.gridOptions.api.setRowData(data);
                 this.gridOptions.api.sizeColumnsToFit();
-
             });
-    }
-
-    onQuickFilterChanged($event) {
-        this.gridOptions.api.setQuickFilter($event.target.value);
     }
 
     ngOnInit() { }
@@ -162,13 +130,17 @@ export class MainmaterialComponent implements OnInit, OnDestroy {
     gridReady(params) {
         params.api.sizeColumnsToFit();
         this.$win.on(this.resizeEvent, () => {
-            setTimeout(() => { params.api.sizeColumnsToFit(); });
+            setTimeout(() => { 
+                params.api.sizeColumnsToFit();
+            });
         });
     }
 
     ngOnDestroy() {
         this.$win.off(this.resizeEvent);
     }
+
+
 
 }
 function countryCellRenderer(params) {
