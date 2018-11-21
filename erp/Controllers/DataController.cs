b39cddcb3f985e4609662a68b3bd7939709b848d
Thai Tran <tranhuynhthai@yahoo.com>
2018-11-21 23:30:15 +0700

    update note

--- erp/Controllers/DataController.cs
+++ erp/Controllers/DataController.cs
@@ -11,41 +11,13 @@ namespace ERP.Controllers
 {
     public class DataController : Controller
     {
-        DataAccessLayer objemployee = new DataAccessLayer();
-
-        [HttpGet("[action]")]
-        [Route("api/Employee/Index")]
-        public IEnumerable<Employee> Index()
-        {
-            return objemployee.GetAllEmployees();
-        }
+        DataAccessLayer  = new DataAccessLayer();
 
         [HttpPost]
-        [Route("api/Employee/Create")]
-        public int Create([FromBody] Employee employee)
+        [Route("api/executeEvent")]
+        public int executeEvent([FromBody])
         {
-            return objemployee.AddEmployee(employee);
-        }
-
-        [HttpGet]
-        [Route("api/Employee/Details/{id}")]
-        public Employee Details(int id)
-        {
-            return objemployee.GetEmployeeData(id);
-        }
-
-        [HttpPut]
-        [Route("api/Employee/Edit")]
-        public int Edit([FromBody]Employee employee)
-        {
-            return objemployee.UpdateEmployee(employee);
-        }
-
-        [HttpDelete]
-        [Route("api/Employee/Delete/{id}")]
-        public int Delete(int id)
-        {
-            return objemployee.DeleteEmployee(id);
+            return true;
         }
     }
 }
